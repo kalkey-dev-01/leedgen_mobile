a105e3785daec51710c1b91df1e2095e4ffe6764
MrJ0bs <ramanhalder12@gmail.com>
2022-08-10 23:44:17 +0530

    --working-with-api-clean-up-required-hooks-req

--- screens/Homescreen.tsx
+++ screens/Homescreen.tsx
@@ -1,61 +1,113 @@
 
 import { useNavigation } from '@react-navigation/native';
 import React, { FC, useLayoutEffect } from 'react'
-import { Text, TextInput, View } from 'react-native'
+import { ActivityIndicator, Text, TextInput, View, ViewProps, TextProps } from 'react-native'
 import { HomeIcon as Home, BellIcon as Notifications, SearchCircleIcon as Search } from 'react-native-heroicons/solid'
 import { Pressable } from "react-native"
 import Animated, { useSharedValue, useAnimatedStyle, withSequence, withTiming, withRepeat } from 'react-native-reanimated';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { RegisterScreen } from './RegisterScreen';
+import axios, { AxiosResponse } from 'axios';
 
 const Tab = createMaterialTopTabNavigator();
 
 const Hscreen = () => {
-    const rotation = useSharedValue(0);
+    const notification = null;
+    const rotation = useSharedValue<number>(0);
     const [names, setName] = React.useState<string>('')
+    const [data, setData] = React.useState<AxiosResponse>()
     const navigation = useNavigation();
     const animatedStyle = useAnimatedStyle(() => {
         return {
             transform: [{ rotateZ: `${rotation.value}deg` }],
         };
     });
-    return (<View className='bg-white'>
-        <View className='flex flex-row items-center px-4 my-5 '>
-            <Text className='text-2xl flex-1 '>Welcome user </Text>
+    return (
+        <View className='bg-white'>
+            <View className='flex flex-row items-center px-4 my-5 '>
+                <Text className='text-2xl flex-1 '>Welcome user </Text>
 
-            <Pressable onPress={() => {
-                rotation.value = withSequence(
-                    withTiming(-10, { duration: 50 }),
-                    withRepeat(withTiming(60, { duration: 150 }), 6, true),
-                    withTiming(0, { duration: 50 })
-                );
-                navigation.navigate('Notifications' as never);
+                <Pressable onPress={() => {
+                    rotation.value = withSequence(
+                        withTiming(-10, { duration: 50 }),
+                        withRepeat(withTiming(60, { duration: 150 }), 6, true),
+                        withTiming(0, { duration: 50 })
+                    );
+                    { notification !== null ? navigation.navigate('Notifications' as never) : undefined }
 
-            }}>
-                <Animated.View style={animatedStyle}>
-                    <Notifications size={25} color='black' />
-                </Animated.View>
+                }}>
+                    <Animated.View style={animatedStyle}>
+                        <Notifications size={25} color='black' />
+                    </Animated.View>
 
-            </Pressable>
+                </Pressable>
 
 
-        </View>
+            </View>
 
-        <Text className='text-left mb-4 px-4 text-lg font-semibold'>Enter the name of the company you want to extract info about</Text>
-        <View className='flex flex-row justify-center align-middle mx-2'>
-            <TextInput className='bg-white border-2 
+            <Text className='text-left mb-4 px-4 text-lg font-semibold'>Enter the name of the company you want to extract info about</Text>
+            <View className='flex flex-row justify-center align-middle mx-2'>
+                <TextInput className='bg-white border-2 
             flex-1 border-black shadow-lg shadow-gray-500 
             mx-2 mb-3 py-2 text-black px-4 rounded-full'
-                value={names}
-                onChangeText={(e) => {
-                    setName(e)
-                }}
-            />
-            <Search color='black' size={50} onPress={() => console.log(names)} />
+                    value={names}
+                    onChangeText={(e) => {
+                        setName(e)
+                    }}
+                />
+                <Search color='black' size={50} onPress={async () => {
+                    console.log(names)
+                    axios.post('https://app.leadwity.com/api-product/incoming-webhook/convert-company-names', {
+                        "api_key": "M1W1N9B0-N0Q9J1U4-O7A8E2H1-H6I3M1Q9",
+                        "company_name": names
+                    }).then((res) => {
+                        setData(res.data)
+                        return data
+                    }).catch((err) => {
+                        console.log(err)
+                    }).finally(() => {
+                        console.log('Post Function')
+                    })
+
+                }} />
+            </View>
+            <View className='bg-black h-full py-3'  >
+                <Text className='text-white font-bold text-xl mx-4'>LeedList</Text>
+                <View>
+                    {
+                        names !== ""
+                            ? <Text className='text-white px-4'>
+                                Press the search icon to execute "{names}"...
+                            </Text>
+
+                            : <Text className='text-white px-4 text-base'>
+                                Your Results will appear here
+                            </Text>
+                    }
+                </View>
+                <View>{data !== undefined
+                    ? <View className=''>
+                        <Text className='text-white text-base font-bold pl-4 ' >{data?.data[0]['name']}</Text>
+                        <Text className='text-white text-base font-bold pl-4 ' > {data?.data[0]['domain']}</Text>
+                        <Text className='text-white text-base font-bold pl-4 ' >{data?.data[1]['name']}</Text>
+                        <Text className='text-white text-base font-bold pl-4 ' > {data?.data[1]['domain']}</Text>
+                        <Text className='text-white text-base font-bold pl-4 ' >{data?.data[2]['name']}</Text>
+                        <Text className='text-white text-base font-bold pl-4 ' > {data?.data[2]['domain']}</Text>
+                        <Text className='text-white text-base font-bold pl-4 ' >{data?.data[3]['name']}</Text>
+                        <Text className='text-white text-base font-bold pl-4 ' > {data?.data[3]['domain']}</Text>
+                        <Text className='text-white text-base font-bold pl-4 ' >{data?.data[4]['name']}</Text>
+                        <Text className='text-white text-base font-bold pl-4 ' > {data?.data[4]['domain']}</Text>
+
+                    </View>
+                    : <Text className='text-white pl-4 font-black pt-10' >It evaluates the responsiveness of the leed</Text>
+                }
+                </View>
+
+
+            </View>
+
         </View>
-        <View>
-        </View>
-    </View>);
+    );
 
 }
 
